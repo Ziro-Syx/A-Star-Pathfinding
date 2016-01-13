b5677159fafad1e7bbffd5d9ef0e906e34eabbe9
SyxRen <syx.ren@outlook.com>
2016-01-13 21:13:02 +0000

    Updated with boxes control and start of managing the rooms

---
+++ scripts/RoomArray_Pointing.js
@@ -0,0 +1,50 @@
+function simulateClick(x, y) {
+    var clickEvent= document.createEvent('MouseEvents');
+    clickEvent.initMouseEvent(
+    'click', true, true, window, 0,
+    0, 0, x, y, false, false,
+    false, false, 0, null
+    );
+    document.elementFromPoint(x, y).dispatchEvent(clickEvent);
+	}
+
+	function foo() {
+		simulateClick(620, 650);		
+		simulateClick(620, 650);
+	}
+	
+	function FindPath() {
+    var a = document.getElementById("Dept").value;
+	var b = document.getElementById("Goal").value;
+	var position;
+	
+	if (a == "Entrance" && b == "Set Class"){
+		simulateClick(620, 650);		
+		simulateClick(620, 650);
+		return;
+	}
+	if (b == "Entrance"){
+		simulateClick(620, 650);
+		position = b;
+		document.getElementById("Dept").value = b;
+		return;
+	}
+	else if (b == "TM1-01"){
+		simulateClick(610, 500);
+		position = b;
+		document.getElementById("Dept").value = b;
+		return;
+	}
+	else if (b == "Blue Zone"){
+		simulateClick(610, 500);
+		position = b;
+		document.getElementById("Dept").value = b;
+		return;
+	}
+	else if (b == "Red Zone"){
+		simulateClick(610, 300);
+		position = b;
+		document.getElementById("Dept").value = b;
+		return;
+	}	
+	}

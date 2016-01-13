d2dab439fdcb9c6594587a327bf826d86f295d6a
SyxRen <SyxRen@users.noreply.github.com>
2016-01-13 23:44:09 +0000

    Update RoomArray_Pointing.js

--- scripts/RoomArray_Pointing.js
+++ scripts/RoomArray_Pointing.js
@@ -1,4 +1,8 @@
-function simulateClick(x, y) {
+function simulateClick(a, b) {
+    var sw = document.width();
+    var val = (sw-550)/2;
+    var x = val + a;
+    var y = b;
     var clickEvent= document.createEvent('MouseEvents');
     clickEvent.initMouseEvent(
     'click', true, true, window, 0,
@@ -17,34 +21,33 @@ function simulateClick(x, y) {
     	var a = document.getElementById("Dept").value;
 	var b = document.getElementById("Goal").value;
 	var position;
-	var sw = document.width();
-	var x = (sw-550)/2;
+	
 	
 	if (a == "Entrance" && b == "Set Class"){
-		simulateClick(x+60, 650);		
-		simulateClick(x+60, 650);
+		simulateClick(60, 650);		
+		simulateClick(60, 650);
 		return;
 	}
 	if (b == "Entrance"){
-		simulateClick(x+60, 650);
+		simulateClick(60, 650);
 		position = b;
 		document.getElementById("Dept").value = b;
 		return;
 	}
 	else if (b == "TM1-01"){
-		simulateClick(x+50, 500);
+		simulateClick(50, 500);
 		position = b;
 		document.getElementById("Dept").value = b;
 		return;
 	}
 	else if (b == "Blue Zone"){
-		simulateClick(x+50, 500);
+		simulateClick(50, 500);
 		position = b;
 		document.getElementById("Dept").value = b;
 		return;
 	}
 	else if (b == "Red Zone"){
-		simulateClick(x+50, 300);
+		simulateClick(50, 300);
 		position = b;
 		document.getElementById("Dept").value = b;
 		return;

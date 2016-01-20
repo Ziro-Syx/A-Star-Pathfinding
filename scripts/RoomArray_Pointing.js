59f73905c7c703bd684764653f0f921fbf507faa
SyxRen <SyxRen@users.noreply.github.com>
2016-01-20 23:03:13 +0000

    Update RoomArray_Pointing.js

--- scripts/RoomArray_Pointing.js
+++ scripts/RoomArray_Pointing.js
@@ -5,7 +5,6 @@
 	var goalchange = [];
 	var leveldept;
 	var levelgoal;
-<<<<<<< HEAD
 	var level;
 
 	function Start(){
@@ -88,38 +87,6 @@
 	goalend[1] = b [1];
 	}
 	
-=======
-	var error;
-	
-	function whereclick(){
-		var x = event.clientX;     // Get the horizontal coordinate
-		var y = event.clientY;     // Get the vertical coordinate
-		var coor = "X coords: " + x + ", Y coords: " + y;
-		//alert(coor);
-	}
-	
-	function levels(){
-		var x = event.clientX;     // Get the horizontal coordinate
-		var y = event.clientY;     // Get the vertical coordinate
-		if(leveldept != levelgoal){
-			if (levelgoal == 0){
-				alert(levelgoal);
-			}else if (levelgoal == 1){
-				alert(levelgoal);
-				$('#Floors').val('1');
-				ChangeFloor(1);
-				
-			}else if (levelgoal >= 2 && levelgoal < 14){
-				alert(levelgoal);
-			}else{
-				return;
-			}
-		}else{
-			alert("You don't need to go on another floor for your path.")
-		}
-	}
-	
->>>>>>> origin/gh-pages
 	function ChangeFloor(x){
 		if(x == 0){
 			$('#boardType').val('FloorZero').change();
@@ -166,68 +133,6 @@
 		
 	}
 	
-<<<<<<< HEAD
-=======
-	function Entrance(){
-		pos(60, 650); //Two click on the entrance to be on the starting point.
-		pos(60, 650);
-	}
-	
-	function Start(){
-		
-		document.getElementById('astar').style.backgroundImage = "url('Images/FloorZero.png')";
-		ChangeFloor(0);
-	}
-
-	
-	function FindPath() {
-	Start();
-	$.when(SetA()).then(SetB()); //Click on location dictated by 1st box, then on the 2nd one.
-	TransData(deptstart[0],deptstart[1]);
-	leveldept = level;
-	if (error == 1){
-		alert("Unknown departure point.")
-		error = 0;
-		}
-	TransData(goalend[0],goalend[1]);
-	levelgoal = level;
-	if (error == 1){
-		alert("Unknown destination point.")
-		error = 0;
-		}
-	if(leveldept !=0 && leveldept != levelgoal){
-		ChangeFloor(leveldept);
-		TransData(deptstart[0],deptstart[1]);
-		TransData(goalend[0],goalend[1]);
-		alert("Your path is on multiple floors. \nTo see the rest of the way to go, click on toggle below the map !")
-	}
-	return;
-	}
-	
-	function MultiFindPath(){
-		
-		
-	}
-	
-	function SetA(){
-	var aw = document.getElementById("Dept").value; //Get value from Text box with same ID
-	var a = [];
-	a = aw.split("-"); // Split with "-"
-	deptstart[0] = a [0];
-	deptstart[1] = a [1];
-	
-	
-	}
-	
-	function SetB(){
-	var bh = document.getElementById("Goal").value; //Get value from Text box with same ID
-	var b = [];
-	b = bh.split("-"); // Split with "-"
-	goalend[0] = b [0];
-	goalend[1] = b [1];
-	}
-	
->>>>>>> origin/gh-pages
 	//Click function by system
 	
 	function simulateClick(x, y) {

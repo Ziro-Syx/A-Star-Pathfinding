b6342f7f5ac841962a92df56695e157038bf53f9
Ziro-Syx <syx.ren@outlook.com>
2016-01-23 16:47:22 +0000

    Last update for buttons

---
+++ scripts/DataRooms.js
@@ -0,0 +1,240 @@
+//Database of the rooms
+	
+	function TransData(zone,room){
+	if (zone == "Entrance"){
+		level = '0';
+		pos(60, 650);
+	}else if (zone == "Blue Zone"){
+		level = '0';
+		pos(50, 500);
+	}else if (zone == "Red Zone"){
+		level = '0';
+		pos(50, 310);
+	}else if (zone == "Yellow Zone"){
+		level = '0';
+		pos(500, 310);
+	}else if (zone == "Green Zone"){
+		level = '0';
+		pos(500, 140);
+	}else if (zone =="TMG"){
+		level = '0';
+		pos(50, 400);
+	}else if (zone =="BEL"){
+		level = '0';
+				if(room >= 0 && room <20){
+					pos(500, 140);
+				}else if(room >= 20 && room <40){
+					pos(450, 140);
+				}else if(room >= 40 && room <60){
+					pos(400, 140);
+				}else{
+					pos(60, 650);
+					alert("Unknown Location/Destination");
+					return;
+				}
+	}else if (zone =="TM1"){
+		level = '1';
+		pos(50, 400);
+	}else if (zone =="T1"){
+		level = '1';
+				if(room >= 0 && room <20){
+					pos(165, 400);
+				}else if(room >= 20 && room <40){
+					pos(95, 400);
+				}else if(room >= 40 && room <60){
+					pos(45, 400);
+				}else{
+					pos(165, 400);
+					alert("Unknown Location/Destination");
+					return;
+				}	
+	}else if (zone =="TM2"){
+		level = '2';
+		pos(50, 400);
+	}else if (zone =="T2"){
+		level = '2';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T3"){
+		level = '3';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T4"){
+		level = '4';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T5"){
+		level = '5';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T6"){
+		level = '6';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T7"){
+		level = '7';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T8"){
+		level = '8';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T9"){
+		level = '9';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T10"){
+		level = '10';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T11"){
+		level = '11';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T12"){
+		level = '12';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else if (zone =="T13"){
+		level = '13';
+				if(room >= 0 && room <5){
+					pos(390, 440);
+				}else if(room >= 5 && room <10){
+					pos(390, 320);
+				}else if(room >= 10 && room <15){
+					pos(135, 320);
+				}else if(room >= 15 && room <20){
+					pos(135, 440);
+				}else{
+					pos(165, 420);
+					alert("Unknown Room at Floor " + level);
+					return;
+				}	
+	}else{
+		pos(60, 650);
+		alert("Unknown Location/Destination");
+		return;	
+	}
+	return;
+	}
--- scripts/board.js
+++ scripts/board.js
@@ -1,6 +1,7 @@
 define(["metrics"], function(metrics) {	
 	var patternBuilders = {
-		LMUWALLS: function(board) {
+		//Wall Layout for the Floor Zero
+		FloorZero: function(board) {
 			var columnIndex;
 			var fromTop;
 			var rowIndex;
@@ -33,7 +34,19 @@ define(["metrics"], function(metrics) {
 				}
 			}
 			for (columnIndex = 36; columnIndex <88; columnIndex ++) {
-				for (rowIndex = 48; rowIndex < 107; rowIndex++) {
+				for (rowIndex = 48; rowIndex < 60; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 58; columnIndex <88; columnIndex ++) {
+				for (rowIndex = 60; rowIndex < 70; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 36; columnIndex <88; columnIndex ++) {
+				for (rowIndex = 70; rowIndex < 107; rowIndex++) {
 					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
 					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
 				}
@@ -69,7 +82,7 @@ define(["metrics"], function(metrics) {
 				}
 			}
 			for (columnIndex = 18; columnIndex <50; columnIndex ++) {
-				for (rowIndex = 89; rowIndex < 102; rowIndex++) {
+				for (rowIndex = 89; rowIndex < 96; rowIndex++) {
 					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
 					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
 				}
@@ -81,11 +94,174 @@ define(["metrics"], function(metrics) {
 				}
 			}
 		},
-		empty: function(board) {
+		//Wall Layout for the Floor One
+		FloorOne: function(board) {
+			var columnIndex;
+			var fromTop;
+			var rowIndex;
+			var calculatedRowIndex;
+
+			fromTop = true;
+
+			for (columnIndex = 0; columnIndex <47; columnIndex ++) {
+				for (rowIndex = 0; rowIndex < 44; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 47; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 0; rowIndex < 15; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 51; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 18; rowIndex < 44; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 11; columnIndex <33; columnIndex ++) {
+				for (rowIndex = 48; rowIndex < 58; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 36; columnIndex <88; columnIndex ++) {
+				for (rowIndex = 48; rowIndex < 60; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 58; columnIndex <88; columnIndex ++) {
+				for (rowIndex = 60; rowIndex < 70; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 36; columnIndex <88; columnIndex ++) {
+				for (rowIndex = 70; rowIndex < 107; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 11; columnIndex <33; columnIndex ++) {
+				for (rowIndex = 62; rowIndex < 78; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 11; columnIndex <23; columnIndex ++) {
+				for (rowIndex = 78; rowIndex < 84; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 11; columnIndex <14; columnIndex ++) {
+				for (rowIndex = 84; rowIndex < 94; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 4; columnIndex <7; columnIndex ++) {
+				for (rowIndex = 44; rowIndex < 115; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 95; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 44; rowIndex < 115; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 18; columnIndex <50; columnIndex ++) {
+				for (rowIndex = 89; rowIndex < 96; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 28; columnIndex <50; columnIndex ++) {
+				for (rowIndex = 82; rowIndex < 107; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+		},
+		//Wall Layout for the upper Floors
+		FloorUpper: function(board) {
+			var columnIndex;
+			var fromTop;
+			var rowIndex;
+			var calculatedRowIndex;
+			fromTop = true;
+			//external walls
+			for (columnIndex = 0; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 70; rowIndex < 120; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 0; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 0; rowIndex < 45; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 0; columnIndex <23; columnIndex ++) {
+				for (rowIndex = 44; rowIndex < 115; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 73; columnIndex <100; columnIndex ++) {
+				for (rowIndex = 44; rowIndex < 115; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			//internal walls left (RED)
+			for (columnIndex = 26; columnIndex <32; columnIndex ++) {
+				for (rowIndex = 48; rowIndex < 58; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 26; columnIndex <32; columnIndex ++) {
+				for (rowIndex = 61; rowIndex < 102; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			//internal walls right (YELLOW)
+			for (columnIndex = 64; columnIndex <70; columnIndex ++) {
+				for (rowIndex = 48; rowIndex < 58; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			for (columnIndex = 64; columnIndex <70; columnIndex ++) {
+				for (rowIndex = 61; rowIndex < 102; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+			//internal walls top (GREEN)
+			for (columnIndex = 26; columnIndex <70; columnIndex ++) {
+				for (rowIndex = 48; rowIndex < 54; rowIndex++) {
+					calculatedRowIndex = fromTop ? rowIndex : board.height-1-rowIndex;
+					board.setMovementCost(columnIndex, calculatedRowIndex, board.movementCosts.impassable);
+				}
+			}
+		},
+		//Empty Layout for Compatibility 
+		Empty: function(board) {
 			
 		}
-	}
 		
+	}
+	
+	//Reset, Clear functions to change layouts
 	return {
 		create: function() {
 			var board  = [];
